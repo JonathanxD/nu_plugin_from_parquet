081e467a8abe0197a31915d94cf9e4e1878d1809
jonathan <jhrldev@gmail.com>
2023-04-12 19:17:50 -0300

    support BigRational alongside decimal and produce a record in the extended mode
    Signed-off-by: jonathan <jhrldev@gmail.com>

--- src/from_parquet.rs
+++ src/from_parquet.rs
@@ -1,8 +1,9 @@
-use bigdecimal::{BigDecimal, FromPrimitive, ToPrimitive};
+use bigdecimal::{BigDecimal, ToPrimitive};
 use bytes::Bytes;
 use chrono::{DateTime, Duration, FixedOffset};
 use nu_protocol::{ShellError, Span, Value};
 use num_bigint::BigInt;
+use num_rational::BigRational;
 use parquet::data_type::Decimal;
 use parquet::file::reader::FileReader;
 use parquet::file::serialized_reader::SerializedFileReader;
@@ -15,10 +16,79 @@ fn parquet_decimal_to_bigdecimal(decimal: &Decimal) -> BigDecimal {
     BigDecimal::new(unscaled_value, decimal.scale() as i64)
 }
 
-fn convert_to_nu(field: &Field, span: Span) -> Value {
-    const DECIMAL_MAX_CONVERSION: u64 = 2u64.pow(53);
-    const DECIMAL_HELP: &'static str = "Currently cannot convert decimals larger than 2^53 to float.";
+fn parquet_decimal_to_bigrational(decimal: &Decimal) -> BigRational {
+    let unscaled_value = BigInt::from_signed_bytes_be(decimal.data());
+    // Should never be negative per-spec, so we just act like it's not. If it does happen to be
+    // negative, we just accept that the value will wrap around, since it's already breaking the
+    // spec.
+    let scale = decimal.scale() as u32;
+    BigRational::new(unscaled_value, BigInt::from(10).pow(scale))
+}
 
+fn parquet_decimal_to_value(decimal: &Decimal, span: Span, opts: &FromParquetOpts) -> Value {
+    const DECIMAL_HELP: &'static str = "cannot convert decimal to float.";
+
+    let f64 = if opts.rational {
+        let rational = parquet_decimal_to_bigrational(decimal);
+        if opts.extended_decimal {
+            let value = rational
+                .to_f64()
+                .map(|f| Value::float(f, span))
+                .unwrap_or_else(|| Value::nothing(span));
+
+            let numerator = rational
+                .numer()
+                .to_i64()
+                .map(|i| Value::int(i, span))
+                .unwrap_or_else(|| Value::nothing(span));
+
+            let denominator = rational
+                .denom()
+                .to_i64()
+                .map(|i| Value::int(i, span))
+                .unwrap_or_else(|| Value::nothing(span));
+
+            let text = Value::string(rational.to_string(), span);
+
+            return Value::record(
+                vec![
+                    format!("value"),
+                    format!("numerator"),
+                    format!("denominator"),
+                    format!("text"),
+                ],
+                vec![value, numerator, denominator, text],
+                span,
+            );
+        }
+        rational.to_f64()
+    } else {
+        let decimal = parquet_decimal_to_bigdecimal(decimal);
+        if opts.extended_decimal {
+            let value = decimal
+                .to_f64()
+                .map(|f64| Value::float(f64, span))
+                .unwrap_or_else(|| Value::nothing(span));
+            return Value::record(
+                vec![String::from("value"), String::from("text")],
+                vec![value, Value::string(decimal.to_string(), span)],
+                span,
+            );
+        }
+        decimal.to_f64()
+    };
+    f64.map(|f| Value::float(f, span))
+        .unwrap_or_else(|| Value::Error {
+            error: Box::new(ShellError::CantConvert {
+                to_type: "f64".into(),
+                from_type: "decimal".into(),
+                span,
+                help: Some(String::from(DECIMAL_HELP)),
+            }),
+        })
+}
+
+fn convert_to_nu(field: &Field, span: Span, opts: &FromParquetOpts) -> Value {
     let epoch: DateTime<FixedOffset> = DateTime::default();
 
     match field {
@@ -58,57 +128,67 @@ fn convert_to_nu(field: &Field, span: Span) -> Value {
             let val = epoch.add(Duration::microseconds(*micros_since_epoch as i64));
             Value::Date { val, span }
         }
-        Field::Decimal(d) => {
-            let decimal = parquet_decimal_to_bigdecimal(&d);
-            let (bigint, _) = decimal.as_bigint_and_exponent();
-
-            if bigint > BigInt::from_u64(DECIMAL_MAX_CONVERSION).unwrap() {
-                None
-            } else {
-                decimal.to_f64()
-            }
-            .map(|f| Value::float(f, span))
-            .unwrap_or_else(|| Value::Error {
-                error: Box::new(ShellError::CantConvert {
-                    to_type: "f64".into(),
-                    from_type: "decimal".into(),
-                    span,
-                    help: Some(String::from(DECIMAL_HELP)),
-                }),
-            })
-        }
-        Field::Group(row) => {
-            convert_parquet_row(row, span)
-        }
-        Field::ListInternal(list) => {
-            Value::list(list.elements().iter().map(|v| convert_to_nu(&v, span)).collect(), span)
-        }
-        Field::MapInternal(map) => {
-            Value::list(map.entries()
+        Field::Decimal(d) => parquet_decimal_to_value(d, span, opts),
+        Field::Group(row) => convert_parquet_row(row, span, opts),
+        Field::ListInternal(list) => Value::list(
+            list.elements()
                 .iter()
-                .map(|(k, v)| Value::list(vec![convert_to_nu(k, span), convert_to_nu(v, span)], span))
-                .collect::<Vec<_>>(), span)
-        }
+                .map(|v| convert_to_nu(&v, span, opts))
+                .collect(),
+            span,
+        ),
+        Field::MapInternal(map) => Value::list(
+            map.entries()
+                .iter()
+                .map(|(k, v)| {
+                    Value::list(
+                        vec![convert_to_nu(k, span, opts), convert_to_nu(v, span, opts)],
+                        span,
+                    )
+                })
+                .collect::<Vec<_>>(),
+            span,
+        ),
     }
 }
 
-fn convert_parquet_row(row: &Row, span: Span) -> Value {
+fn convert_parquet_row(row: &Row, span: Span, opts: &FromParquetOpts) -> Value {
     let mut cols = vec![];
     let mut vals = vec![];
     for (name, field) in row.get_column_iter() {
         cols.push(name.clone());
-        vals.push(convert_to_nu(field, span.clone()));
+        vals.push(convert_to_nu(field, span.clone(), opts));
     }
     Value::Record { cols, vals, span }
 }
 
-pub fn from_parquet_bytes(bytes: Vec<u8>, span: Span) -> Value {
+#[derive(Debug, Clone, Copy)]
+pub struct FromParquetOpts {
+    /// Uses a [`Value::Record`] to represent the decimal value instead of a [`Value::Float`].
+    ///
+    /// This means that the decimal value will be represented as a record with two or three fields
+    /// depending on the whether [`FromParquetOpts::rational`] is set or not.
+    ///
+    /// The fields are:
+    /// - For BigDecimal
+    ///   - `value`: The decimal value as an approximated float value.
+    ///   - `text`: The decimal value as a string (integrity preserved).
+    /// - For BigRational
+    ///   - `numerator`: The numerator of the decimal value as an i64.
+    ///   - `denominator`: The denominator of the decimal value as an i64.
+    ///   - `text`: The decimal value as a string (integrity preserved).
+    pub extended_decimal: bool,
+    /// Uses [`BigRational`] instead of [`BigDecimal`] for decimal values.
+    pub rational: bool,
+}
+
+pub fn from_parquet_bytes(bytes: Vec<u8>, span: Span, opts: &FromParquetOpts) -> Value {
     let cursor = Bytes::from(bytes);
     let reader = SerializedFileReader::new(cursor).unwrap();
     let mut iter = reader.get_row_iter(None).unwrap();
     let mut vals = Vec::new();
     while let Some(record) = iter.next() {
-        let row = convert_parquet_row(&record, span);
+        let row = convert_parquet_row(&record, span, opts);
         vals.push(row);
     }
     Value::List { vals, span }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod from_parquet;
 
-use nu_plugin::{serve_plugin, Plugin, JsonSerializer, EvaluatedCall, LabeledError};
-use nu_protocol::{Value, PluginSignature};
+use nu_plugin::{serve_plugin, EvaluatedCall, JsonSerializer, LabeledError, Plugin};
+use nu_protocol::{PluginSignature, Value};
+
+use crate::from_parquet::FromParquetOpts;
 
 struct FromParquet;
 
 impl FromParquet {
+    const EXTENDED_FORMAT_OPTION: &'static str = "extended-decimal";
+    const RATIONAL_OPTION: &'static str = "rational";
+
     fn new() -> Self {
         Self {}
     }
@@ -16,12 +21,14 @@ impl Plugin for FromParquet {
         vec![
             PluginSignature::build("from parquet")
             .usage("Convert from .parquet binary into table")
+            .switch(Self::EXTENDED_FORMAT_OPTION, "extends the decimal output to be a table instead of a float64", Some('x'))
+            .switch(Self::RATIONAL_OPTION, "uses BigRational instead of BigDecimal. When used with `-x` produces the Ratio in the `text` field instead of the decimal value", Some('r'))
             .filter()
         ]
     }
 
     fn run(
-        &mut self, 
+        &mut self,
         name: &str,
         call: &EvaluatedCall,
         input: &Value,
@@ -29,7 +36,15 @@ impl Plugin for FromParquet {
         assert_eq!(name, "from parquet");
         match input {
             Value::Binary { val, span } => {
-                Ok(crate::from_parquet::from_parquet_bytes(val.clone(), span.clone()))
+                let opts = FromParquetOpts {
+                    extended_decimal: call.has_flag(Self::EXTENDED_FORMAT_OPTION),
+                    rational: call.has_flag(Self::RATIONAL_OPTION),
+                };
+                Ok(crate::from_parquet::from_parquet_bytes(
+                    val.clone(),
+                    span.clone(),
+                    &opts,
+                ))
             }
             v => {
                 return Err(LabeledError {
